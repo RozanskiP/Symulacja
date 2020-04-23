38626e1f193fb7c35b05ccf9a356b30b854e9673
Rand2525 <60777256+Rand2525@users.noreply.github.com>
2020-04-23 20:16:24 +0200

    Dodane budynki

---
+++ Symulacja/src/main/java/Budynek/Mennica.java
@@ -0,0 +1,12 @@
+package Budynek;
+
+public class Mennica extends Budynek {
+
+    private int dodatkoweTowaryPlus;
+
+    public Mennica (int XBudynek, int YBudynek){
+        setXBudynek(XBudynek);
+        setYBudynek(YBudynek);
+    }
+    public void plusTowary() {}
+}
