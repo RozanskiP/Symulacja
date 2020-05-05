9aec8376d9ed060b1a16c01949466bd98aafb11f
Pawel <pawir4@wp.pl>
2020-05-05 23:49:22 +0200

    Naprawa Gradle

--- Symulacja/src/main/java/RandomoweLiczby/GeneratorRandom.java
+++ Symulacja/src/main/java/RandomoweLiczby/GeneratorRandom.java
@@ -20,12 +20,4 @@ public class GeneratorRandom {
 		
 		return a;
 	}
-	
-	public static void main(String[] args) {
-		for(int i =0 ;i <10;i++) {
-			int a = GeneratorRandom.RandomOd1(3);
-			System.out.println(a);
-		}
-	}
-	
 }
