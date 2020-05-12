9cf56574356cb696a0cc81e7f4293e5c3a08877e
Pawel <pawir4@wp.pl>
2020-05-12 11:40:57 +0200

    1.1

--- Symulacja/src/main/java/RandomoweLiczby/GeneratorRandom.java
+++ Symulacja/src/main/java/RandomoweLiczby/GeneratorRandom.java
@@ -3,16 +3,36 @@ package RandomoweLiczby;
 import java.util.Random;
 //Generator losowych liczb w roznych zakresach
 public class GeneratorRandom {
+	
+	private static final int ZiarnoGeneratora = 47;
+	
 	//Generator losowych liczb od 1 do Zakres
-	public static int RandomOd1(int Zakres) {
+	public static int RandomOd1Losowe(int Zakres) {
 		Random rand = new Random();
 		int a = rand.nextInt(Zakres)+1;
 		
 		return a;
 	}
+	
+	//Generator losowy liczb od 0 do Zakres
+	public static int RandomOd0Losowe(int Zakres) {
+		Random rand = new Random();
+		int a = rand.nextInt(Zakres);
+		
+		return a;
+	}
+	
+	//Generator losowych liczb od 1 do Zakres
+		public static int RandomOd1(int Zakres) {
+			Random rand = new Random(ZiarnoGeneratora);
+			int a = rand.nextInt(Zakres)+1;
+			
+			return a;
+	}
+		
 	//Generator losowy liczb od 0 do Zakres
 	public static int RandomOd0(int Zakres) {
-		Random rand = new Random();
+		Random rand = new Random(ZiarnoGeneratora);
 		int a = rand.nextInt(Zakres);
 		
 		return a;
---
+++ Symulacja/src/main/java/Tests/TestBudynki.java
@@ -0,0 +1,72 @@
+package Tests;
+
+//import static org.junit.Assert.*;
+
+//import org.junit.Test;
+
+import Budynki.*;
+import RandomoweLiczby.GeneratorRandom;
+import Towar.*;
+
+public class TestBudynki {
+/*
+	@Test
+	public void testMennicaIlosc() {
+		Towar towar1 = new Towar(0, 0);
+		Towar towar1kopia = new Towar(0, 0);
+		Mennica.PlusTowary(towar1);
+		//Sprawdzanie czy Budynek dodaje odpowiednia ilosc towarow
+		assertEquals(towar1.getJedzenie(), towar1kopia.getJedzenie()+3);
+		assertEquals(towar1.getMaterialy(), towar1kopia.getMaterialy()+3);
+		assertEquals(towar1.getNarzedzia(), towar1kopia.getNarzedzia()+3);
+		assertEquals(towar1.getTowary(), towar1kopia.getTowary()+3);
+		assertEquals(towar1.getUbrania(), towar1kopia.getUbrania()+3);
+		assertEquals(towar1.getZloto(), towar1kopia.getZloto()+3);
+	}
+	
+	@Test
+	public void testWarsztatIlosc() {
+		Towar towar1 = new Towar(0, 0);
+		Towar towar1kopia = new Towar(0, 0);
+		Warsztat.WymanazajTowary(towar1);
+		//Sprawdzanie czy Budynek wymnaza w odpowiedni sposob
+		assertEquals(towar1.getJedzenie(), towar1kopia.getJedzenie()*2);
+		assertEquals(towar1.getMaterialy(), towar1kopia.getMaterialy()*2);
+		assertEquals(towar1.getNarzedzia(), towar1kopia.getNarzedzia()*2);
+		assertEquals(towar1.getTowary(), towar1kopia.getTowary()*2);
+		assertEquals(towar1.getUbrania(), towar1kopia.getUbrania()*2);
+		assertEquals(towar1.getZloto(), towar1kopia.getZloto()*2);
+	}
+	
+	@Test
+	public void testMennicaWarsztatPolozenie() {
+		assertTrue(polozenieMennica());
+		assertTrue(polozenieWarsztat());
+	}
+	
+	
+	public boolean polozenieWarsztat() {
+		int X=20;
+		int Y=20;
+		Warsztat warsztat = new Warsztat(GeneratorRandom.RandomOd1Losowe(X), GeneratorRandom.RandomOd1Losowe(Y));
+		if(warsztat.getXBudynek() >= 0 && warsztat.getYBudynek() <= X) {
+			if(warsztat.getYBudynek() >= 0 && warsztat.getYBudynek() <= Y) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean polozenieMennica() {
+		int X=20;
+		int Y=20;
+		Mennica mennica = new Mennica(GeneratorRandom.RandomOd1Losowe(X), GeneratorRandom.RandomOd1Losowe(Y));
+		if(mennica.getXBudynek() >= 0 && mennica.getYBudynek() <= X) {
+			if(mennica.getYBudynek() >= 0 && mennica.getYBudynek() <= Y) {
+				return true;
+			}
+		}
+		return false;
+	}
+	*/
+}
